179a91b74260f007b045ad5c4e461a3202a4534d
Pierre-Rene-07 <80160100+Pierre-Rene-07@users.noreply.github.com>
2021-03-06 21:36:15 +0100

    Update start.sh

--- Spigot/start.sh
+++ Spigot/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 while true; do
-    java -server -Xmx2G -DenableDebugMethodProfiler=true -jar spigot.jar
+    java -server -Xmx2G -DenableDebugMethodProfiler=true -jar spigot.jar -nogui
 done;
